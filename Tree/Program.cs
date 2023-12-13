e438786dd96bb3ae401889904fc5f1bd7decfde5
DesislavaTsocheva <desislava_tsocheva@abv.bg>
2023-12-13 13:05:26 +0200

    first commit

---
+++ Tree/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Trees<int> tree = new Trees<int>
+                (
+                7, new Trees<int>(19,
+                new Trees<int>(1), 
+                new Trees<int>(12), 
+                new Trees<int>(31)
+                ),
+                new Trees<int>(21),
+                new Trees<int>
+                (
+                    14, 
+                new Trees<int>(23,
+                new Trees<int>(6)
+                )
+                ));
+        }
+    }
+}
---
+++ Tree/Trees.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    public class Trees<T>
+    {
+        private T value;
+        private IList<Trees<T>> children;
+        public Trees(T value, params Trees<T>[] children)
+        {
+            this.value = value;
+            this.children = children;
+        }
+        public void Print(int indent=0)
+        {
+            Console.WriteLine(new string(' ',2*indent));
+            Console.WriteLine(this.value);
+            foreach (var child in this.children)
+            {
+                child.Print(indent+1);    
+            }
+        }
+    }
+}
