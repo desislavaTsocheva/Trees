558a1369b1891995c1385a71417e8e3af3b9c819
DesislavaTsocheva <desislava_tsocheva@abv.bg>
2024-01-21 12:42:23 +0200

    Tree- example

---
+++ Tree/Tree/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    public class Program
+    {
+        private static Dictionary<int, TreeClass<int>> nodeByValue = new Dictionary<int, TreeClass<int>>();
+        private static TreeClass<int> GetTreeNodeByValue(int value)
+        {
+            if(!nodeByValue.ContainsKey(value))
+            {
+                nodeByValue[value] = new TreeClass<int>(value, null);
+            }
+            return nodeByValue[value];  
+        }
+        private static void AddEdge(int parent,int child)
+        {
+            TreeClass<int>parentNode=GetTreeNodeByValue(parent);
+            TreeClass<int>childNode=GetTreeNodeByValue(child);
+            parentNode.AddChild(childNode);
+            childNode.SetParent(parentNode);
+        }
+        private static void ReadTree()
+        {
+            int nodeCount=int.Parse(Console.ReadLine());
+            for (int i = 0; i < nodeCount; i++)
+            {
+                string[] edge = Console.ReadLine().Split(' ').ToArray();
+                AddEdge(int.Parse(edge[0]), int.Parse(edge[1]));
+            }
+        }
+        private static TreeClass<int> GetRoot()
+        {
+            TreeClass<int> root=nodeByValue.Values.Where(x=>x.Parent==null).FirstOrDefault();
+            return root;
+        }
+        static void Main(string[] args)
+        {
+         ReadTree();
+            TreeClass<int>.PrintTree(GetRoot(),0);
+        }
+    }
+}
---
+++ Tree/Tree/TreeClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    public class TreeClass<T>
+    {
+        public T Value { get; set; }
+        public TreeClass<T> Parent { get; set; }
+        public List<TreeClass <T>> Children { get; set;}
+
+        public TreeClass(T value, params TreeClass<T>[] children)
+        {
+           this.Value = value;
+            if (children == null)
+            {
+                this.Children = new List<TreeClass<T>>();
+            }
+            else
+            {
+                foreach (var child in children)
+                {
+                    this.Parent = this;
+                }
+            }
+        } 
+        public void AddChild(TreeClass<T> newChild)
+        {
+            this.Children.Add(newChild);
+        }
+        public void SetParent(TreeClass<T> parent)
+        {
+            this.Parent = parent;
+        }
+        public static void PrintTree(TreeClass<T> node,int offset)
+        {
+            Console.WriteLine("{0}{1}",new string (' ',offset*2),node.Value);
+            foreach (var child in node.Children)
+            {
+                PrintTree(child, offset + 1);
+            }    
+        }
+
+    }
+}
